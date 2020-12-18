35cd8cefb503c6e249956d567115a666023d6786
Nathan Miller <natem31190@gmail.com>
2020-12-18 18:29:25 -0500

    commit tree b0e208ed3ff5a30b428f4c23b3757849b1ad4c09

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,31 @@
-def oxford_comma(array)
+# 2 strings will have an "and"
+# > 2 string will have commas and a "and"
 
-end
+    def oxford_comma(array) 
+        if array.length == 1
+            array.join
+        elsif array.length == 2
+            array.join(" and ")
+        else 
+            [array[0..-2].join(', '), array.last].join(', and ')
+            
+        end
+    end
+
+
+
+
+
+
+
+
+
+
+#     *def oxford_comma(array)
+#     *if array.length == 2
+#         "#{array.first} and #{array.last}"
+#     elsif array.length > 2
+#     else 
+#         array.firstend
+
+# end
